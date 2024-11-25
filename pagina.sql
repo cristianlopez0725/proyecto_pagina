f0d2622da5152f9ed90f970bb2645d5038090a1d
cristianlopez0725 <cristianandrey725@gmail.com>
2024-11-24 22:22:33 -0500

    modificaciones

--- pagina.sql
+++ pagina.sql
@@ -16,29 +16,63 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `contactos`
+-- Table structure for table `estudios`
 --
 
-DROP TABLE IF EXISTS `contactos`;
+DROP TABLE IF EXISTS `estudios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `contactos` (
-  `id_contacto` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` text NOT NULL,
-  `correo_contacto` text NOT NULL,
-  `mensaje` text NOT NULL,
-  `fecha_envio_mensaje` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id_contacto`)
+CREATE TABLE `estudios` (
+  `est_id` int(11) NOT NULL AUTO_INCREMENT,
+  `usu_id` int(11) DEFAULT NULL,
+  `institucion` varchar(255) DEFAULT NULL,
+  `titulo` varchar(255) DEFAULT NULL,
+  `fecha_inicio` date DEFAULT NULL,
+  `fecha_fin` date DEFAULT NULL,
+  `est` int(11) DEFAULT NULL,
+  PRIMARY KEY (`est_id`),
+  KEY `usu_id` (`usu_id`),
+  CONSTRAINT `estudios_ibfk_1` FOREIGN KEY (`usu_id`) REFERENCES `usuarios` (`usu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `contactos`
+-- Dumping data for table `estudios`
 --
 
-LOCK TABLES `contactos` WRITE;
-/*!40000 ALTER TABLE `contactos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `contactos` ENABLE KEYS */;
+LOCK TABLES `estudios` WRITE;
+/*!40000 ALTER TABLE `estudios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estudios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `experiencias`
+--
+
+DROP TABLE IF EXISTS `experiencias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `experiencias` (
+  `exp_id` int(11) NOT NULL AUTO_INCREMENT,
+  `usu_id` int(11) DEFAULT NULL,
+  `empresa` varchar(255) DEFAULT NULL,
+  `puesto` varchar(255) DEFAULT NULL,
+  `fecha_inicio` date DEFAULT NULL,
+  `fecha_fin` date DEFAULT NULL,
+  `est` int(11) DEFAULT NULL,
+  PRIMARY KEY (`exp_id`),
+  KEY `usu_id` (`usu_id`),
+  CONSTRAINT `experiencias_ibfk_1` FOREIGN KEY (`usu_id`) REFERENCES `usuarios` (`usu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `experiencias`
+--
+
+LOCK TABLES `experiencias` WRITE;
+/*!40000 ALTER TABLE `experiencias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `experiencias` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -79,7 +113,7 @@ CREATE TABLE `social_media` (
   `socmed_url` text DEFAULT NULL,
   `est` int(11) DEFAULT NULL,
   PRIMARY KEY (`socmed_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,6 +122,7 @@ CREATE TABLE `social_media` (
 
 LOCK TABLES `social_media` WRITE;
 /*!40000 ALTER TABLE `social_media` DISABLE KEYS */;
+INSERT INTO `social_media` VALUES (1,'fab fa-facebook','https://www.facebook.com/tu_pagina',1),(2,'fab fa-instagram','https://www.instagram.com/tu_perfil',1);
 /*!40000 ALTER TABLE `social_media` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -117,7 +152,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (6,'Juan','Perez','Lopez','juan.perez@example.com','pass123','1234567890',1),(7,'Maria','Gomez','Sanchez','maria.gomez@example.com','securepass','0987654321',1),(8,'Carlos','Hernandez','Martinez','carlos.hernandez@example.com','mypassword','1122334455',0),(9,'Ana','Ruiz','Diaz','ana.ruiz@example.com','ana2024','5566778899',1),(10,'Luis','Torres','Ramirez','luis.torres@example.com','luispass','6677889900',0),(11,'Cristian','Lopez','Ortiz','cristian@ejemplo.com','Cr123456','987654321',1);
+INSERT INTO `usuarios` VALUES (6,'Juan','Perez','Lopez','juan.perez@example.com','pass123456','1234567890',1),(7,'Maria','Gomez','Sanchez','maria.gomez@example.com','securepass','0987654321',1),(8,'Carlos','Hernandez','Martinez','carlos.hernandez@example.com','mypassword','1122334455',0),(9,'Ana','Ruiz','Diaz','ana.ruiz@example.com','ana2024','5566778899',1),(10,'Luis','Torres','Ramirez','luis.torres@example.com','luispass','6677889900',0),(11,'Cristian','Lopez','Ortiz','cristian@ejemplo.com','Cr123456\r\n','987654321',1);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -130,4 +165,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-17 15:45:22
+-- Dump completed on 2024-11-24 22:21:16
